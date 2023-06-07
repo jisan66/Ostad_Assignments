c067127e52c376219829db0fe6d2c43963636f9e
Zesan Saha <41072153+jisan66@users.noreply.github.com>
2023-06-07 23:39:03 +0600

    Create Assignment_5.dart

---
+++ Assignment_5.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tabbed App Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  int _currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+    _tabController.addListener(() {
+      setState(() {
+        _currentIndex = _tabController.index;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tabbed App'),
+        centerTitle: true,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Navigation Drawer',
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Page 1'),
+              onTap: () {
+                _tabController.animateTo(0);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Page 2'),
+              onTap: () {
+                _tabController.animateTo(1);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Page 3'),
+              onTap: () {
+                _tabController.animateTo(2);
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          Center(
+            child: Text(
+              'Page 1',
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+          Center(
+            child: Text(
+              'Page 2',
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+          Center(
+            child: Text(
+              'Page 3',
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+            _tabController.animateTo(index);
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Page 1',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Page 2',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'Page 3',
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: _getFABColor(_currentIndex),
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Color _getFABColor(int currentIndex) {
+    switch (currentIndex) {
+      case 0:
+        return Colors.red;
+      case 1:
+        return Colors.green;
+      case 2:
+        return Colors.blue;
+      default:
+        return Colors.blue;
+    }
+  }
+}
